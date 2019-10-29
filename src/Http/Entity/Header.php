665495c70340ef7480b5196e6806f56fd69ab385
Gor Mkhitaryan <gor.mhitaryan@list.ru>
2019-10-29 18:03:06 +0300

    Added header class

--- src/Http/Entity/Header.php
+++ src/Http/Entity/Header.php
@@ -1,10 +1,54 @@
 <?php
 
-
 namespace Chiven\Http\Entity;
 
-
+/**
+ * Class Header
+ * @package Chiven\Http\Entity
+ */
 class Header
 {
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $value;
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue(string $value): void
+    {
+        $this->value = $value;
+    }
+
 
 }
