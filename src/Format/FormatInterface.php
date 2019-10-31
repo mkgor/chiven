9b3334a1ce09aabacad508a710eb8e73165e8c41
Gor Mkhitaryan <gor.mhitaryan@list.ru>
2019-10-31 14:37:17 +0300

    Format first version finished

--- src/Format/FormatInterface.php
+++ src/Format/FormatInterface.php
@@ -2,13 +2,28 @@
 
 namespace Chiven\Format;
 
+use Chiven\Http\Response\AbstractResponse;
+
 /**
  * Interface FormatInterface
  * @package Chiven\Format
  */
 interface FormatInterface
 {
-    public function __construct();
+    /**
+     * @param int $errno The first parameter, errno, contains the level of the error raised, as an integer.
+     * @param string $errstr  The second parameter, errstr, contains the error message, as a string.
+     * @param string $errfile  The third parameter is optional, errfile, which contains the filename that the error was raised in, as a string.
+     * @param int $errline The fourth parameter is optional, errline, which contains the line number the error was raised at, as an integer.
+     * @return mixed
+     */
+    public function errorHandler(int $errno , string $errstr, string $errfile, int $errline);
 
-    public function errorHandler();
+    /**
+     * Decorates response body
+     *
+     * @param AbstractResponse $response
+     * @return mixed
+     */
+    public function responseDecorator(AbstractResponse $response);
 }
--- src/Format/Json.php
+++ src/Format/Json.php
@@ -1,18 +1,81 @@
 <?php
 
-
 namespace Chiven\Format;
 
+use Chiven\Http\Entity\Header;
+use Chiven\Http\Response\AbstractResponse;
+use Chiven\Http\Response\Response;
 
+/**
+ * Class Json
+ * @package Chiven\Format
+ */
 class Json implements FormatInterface
 {
-    public function __construct()
+    /**
+     * @var array
+     */
+    private $errorsArray;
+
+    /**
+     * @return mixed
+     */
+    public function getErrorsArray()
     {
-        /** Setting up error handler, so it should render all errors in specified format */
-        set_error_handler([$this, 'errorHandler']);
+        return $this->errorsArray;
     }
 
-    public function errorHandler()
+    /**
+     * @param mixed $errorsArray
+     */
+    public function setErrorsArray($errorsArray): void
     {
+        $this->errorsArray = $errorsArray;
     }
+
+    /**
+     * @param array $errorArray
+     */
+    public function addError($errorArray): void
+    {
+        $this->errorsArray[] = $errorArray;
+    }
+
+    /**
+     * @param AbstractResponse $response
+     * @return false|mixed|string
+     */
+    public function responseDecorator(AbstractResponse $response)
+    {
+        if($response->getHeaderByName('Content-type') == null) {
+            $response->addHeader(new Header('Content-type', 'application/json'));
+        }
+
+        foreach($response->getHeaders() as $header) {
+            $header->assignHeader();
+        }
+
+        return is_array($response->getBody()) ? json_encode($response->getBody()) : json_encode([$response->getBody()]);
+    }
+
+    /**
+     * @param int $errno The first parameter, errno, contains the level of the error raised, as an integer.
+     * @param string $errstr The second parameter, errstr, contains the error message, as a string.
+     * @param string $errfile The third parameter is optional, errfile, which contains the filename that the error was raised in, as a string.
+     * @param int $errline The fourth parameter is optional, errline, which contains the line number the error was raised at, as an integer.
+     * @return mixed
+     */
+    public function errorHandler(int $errno, string $errstr, string $errfile, int $errline)
+    {
+        $response = new Response();
+        $response->setBody([
+            'errno' => $errno,
+            'error' => $errstr,
+            'file' => $errfile,
+            'line' => $errline
+        ]);
+
+        exit($this->responseDecorator($response));
+    }
+
 }
