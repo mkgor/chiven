866f2762d071ad43d28714605b373ca7f928ffbb
Gor Mkhitaryan <gor@difocus.ru>
2019-11-04 20:10:49 +0300

    Added repositories to make access to files and headers objects easier

---
+++ src/Http/Repository/FileRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Chiven\Http\Repository;
+
+use Chiven\Http\Entity\Insertable;
+
+/**
+ * Class FileRepository
+ *
+ * @package Chiven\Http\Repository
+ */
+class FileRepository extends AbstractRepository
+{
+
+}
---
+++ src/Http/Repository/HeaderRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Chiven\Http\Repository;
+
+/**
+ * Class HeaderRepository
+ *
+ * @package Chiven\Http\Repository
+ */
+class HeaderRepository extends AbstractRepository
+{
+
+}
