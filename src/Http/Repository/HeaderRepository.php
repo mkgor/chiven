393ecfcf0462203b0dd18e29b9e1e85ce109e9c3
Gor Mkhitaryan <gor.mhitaryan@list.ru>
2019-11-11 18:14:49 +0300

    Added setContainer method to delete duplicating headers

--- src/Http/Repository/HeaderRepository.php
+++ src/Http/Repository/HeaderRepository.php
@@ -2,6 +2,9 @@
 
 namespace Chiven\Http\Repository;
 
+use Chiven\Http\Entity\Header;
+use Chiven\Http\Entity\Insertable;
+
 /**
  * Class HeaderRepository
  *
@@ -9,5 +12,38 @@ namespace Chiven\Http\Repository;
  */
 class HeaderRepository extends AbstractRepository
 {
+    /**
+     * @param Insertable $object
+     *
+     * @return void
+     */
+    public function insert(Insertable $object): void
+    {
+        if(parent::findBy('name', $object->getName()) instanceof Insertable) {
+            parent::remove('name', $object->getName());
+        }
 
+        parent::insert($object);
+    }
+
+    public function setContainer(array $container): void
+    {
+        $namesArray = [];
+
+        /** @var Header $item */
+        foreach($container as $i => $item) {
+            if(in_array($item->getName(), $namesArray)) {
+                foreach($container as $k => $subItem) {
+                    if($subItem->getName() == $item->getName()) {
+                        unset($container[$k]);
+                        break;
+                    }
+                }
+            } else {
+                $namesArray[$i] = $item->getName();
+            }
+        }
+
+        parent::setContainer($container);
+    }
 }
